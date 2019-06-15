be6bae451bb09446743149c9fe3f8a0b78bdad80
Charly921 <44937626+Charly921@users.noreply.github.com>
2019-06-15 15:41:45 -0500

    version 1.3.2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyAqXzmBod74YY3OpYUXQmbuivmiPX9-onQ",
-    authDomain: "encuesta-huerta.firebaseapp.com",
-    databaseURL: "https://encuesta-huerta.firebaseio.com",
-    projectId: "encuesta-huerta",
-    storageBucket: "encuesta-huerta.appspot.com",
-    messagingSenderId: "81949247931"
-  }
+  production: true
 };
